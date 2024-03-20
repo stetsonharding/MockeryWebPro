447ab5215879a62e2ad739e7380ca98bd97017c1
stetson <stetson2001@msn.com>
2024-03-20 07:59:23 -0700

    hero logo, title, login, and CTA

---
+++ app/ui/home/components/HeroHeadline.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeroHeadline() {
+  return (
+    <div className="flex w-full my-8 lg:w-1/3 flex-col lg:flex-row lg:items-start p-5 md:px-10 break-words">
+    <p className="text-white text-lg md:text-xl font-satoshi">
+      Welcome to <span className="text-home-orange font-bold">Mockery Hub</span> - The Ultimate Mockery Solution! Easily Create, Manage, and Implement Mock Data for Your Software Development and testing.
+    </p>
+  </div>
+  
+  
+  );
+}
+
+export default HeroHeadline;
