fe0260a7c9ba496cf9d5575e6939c9fe95ee1ac7
stetson <stetson2001@msn.com>
2024-03-22 07:27:52 -0700

    Features Data added and customized

--- app/page.jsx
+++ app/page.jsx
@@ -34,7 +34,10 @@ export default function Page() {
       {/* == */}
       </div>
       <SectionTitle title="Key Features"/>
-      <KeyFeatures />
+      <KeyFeatures imgSrc="/assets/icons/CreateMock.png" imageTitle="Create Mocks" featureDescription="Create a mock using our friendly mockery interface. With just a few clicks, you'll be able to create mock data effortlessly, thanks to our intuitive tools designed with you in mind." />
+      <KeyFeatures even="yes" imgSrc="/assets/icons/DataStored.png" imageTitle="Data Stored" featureDescription="Mock data and endpoint information have been seamlessly integrated into the Mockery service, where they are securely stored within our sophisticated backend Cosmos DB system."/>
+      <KeyFeatures imgSrc="/assets/icons/SendHeader.png" imageTitle="Send Header to Mockery Hub" featureDescription="Clients simply sends over header information to Mockery specifying the endpoints and tags they need, and voilà, the mock data tailored to their requirements is promptly returned."/>
+      <KeyFeatures even="yes" imgSrc="/assets/icons/DataNeeded.png" imageTitle="Data when you need it" featureDescription="Services that integrate with Mockery utilize the Mockery APIs to seamlessly fetch tailored mock data for precise endpoints."/>
       <SectionTitle title="How it Works"/>
       
     </main>
--- app/ui/home/components/KeyFeatures.jsx
+++ app/ui/home/components/KeyFeatures.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+import clsx from "clsx";
 
-
-function KeyFeatures() {
+function KeyFeatures(props) {
   return (
-    <div className="flex flex-col md:flex-row">
-        {/* Image and title */}
-        <div className="flex justify-center items-center flex-col md:w-1/2">
+    <div
+      className={clsx("flex flex-col md:flex-row", {
+        "md:flex-row-reverse": props.even === "yes",
+        "bg-gray-200": props.even === "yes"
+      })}
+    >
+      {/* Image and title */}
+      <div className="flex justify-center items-center flex-col md:w-1/2">
         <Image
-        src="/assets/icons/CreateMock.png"
-        alt="Creativity"
-        width={400}
-        height={400}
-        className="object-contain"
+          src={props.imgSrc}
+          alt="Creativity"
+          width={400}
+          height={400}
+          className="object-contain"
         />
-        <p className="text-home-orange font-bold text-[20px] tracking-wider pr-4 m-4">Create Mocks</p>
-        </div>
-        {/* Feature description */}
-        <div className="flex justify-center items-center md:w-1/2">
-        <p className="text-blue-700 w-1/2 font-mono font-semibold text-sm m-5 md:text-lg  ">Create a mock using our friendly mockery interface. With just a few clicks, you'll be able to create mock data effortlessly, thanks to our intuitive tools designed with you in mind.</p>
-        </div>
+        <p className="text-home-orange font-bold text-[20px] tracking-wider p-3">
+          {props.imageTitle}
+        </p>
+      </div>
+      {/* Feature description */}
+      <div className="flex justify-center items-center md:w-1/2">
+        <p className="text-blue-700 w-1/2 font-mono font-semibold text-sm m-5 md:text-lg">
+          {props.featureDescription}
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default KeyFeatures
+export default KeyFeatures;
